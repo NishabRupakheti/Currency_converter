9e7ed926e9f3de771b65c427c5c55277476e62f1
Nishab <nishabrupakheti2@gmail.com>
2024-12-25 13:38:25 +0200

    Styling and responsiveness enhanced

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -5,7 +5,7 @@ import styles from "../styles/inputGroup.module.css";
 
 const Main = () => {
   return (
-    <div className={`container w-75 ${styles['inputGroup']}  `}>
+    <div className={`container-fluid w-sm-100 w-md-75 w-lg-50 ${styles['inputGroup']}`}>
       <div className="card">
         <div className="card-body">
             <Input />

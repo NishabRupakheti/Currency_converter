4c543b214ba34c09c8deab2cac21f0d491fef22c
Nishab <nishabrupakheti2@gmail.com>
2026-01-02 13:38:52 +0200

    added a base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5741,
-  }
+  base: '/Currency_Converter/' // Set the base path for the project
 })
 
 

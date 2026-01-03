9e9ba90a3b8b2414ba29af8c16a53b437cc07845
Nishab <nishabrupakheti2@gmail.com>
2026-01-03 10:18:40 +0200

    Fix: correct base path case sensitivity

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Currency_Converter/' // Set the base path for the project
+  base: '/Currency_converter/' // Set the base path for the project
 })
